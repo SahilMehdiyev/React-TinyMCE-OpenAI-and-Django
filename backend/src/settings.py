362857a47e4a7d9c7fd06edbd7c63de4ecaba0eb
SahilMehdiyev <branchsahil@gmail.com>
2025-02-16 00:24:39 +0400

    crated frontend setup

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from .env import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)kg@u-*eq-hgu-u6y65s+upu-e+j-f!+vu0%ac7u%+z+i!--j*'
+SECRET_KEY = config('SECRET_KEY',cast=str,default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool,default=False)
 
 ALLOWED_HOSTS = []
 
